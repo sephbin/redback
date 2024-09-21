8225c12809c5edac629214dea1bb4689e6638af7
Andrew Butler <sephbin@gmail.com>
2024-09-21 23:11:40 +1000

    Redback 0.3.18

--- rh8Project/build/rh8/src/Redback-Beta.GH/AssemblyInfo.cs
+++ rh8Project/build/rh8/src/Redback-Beta.GH/AssemblyInfo.cs
@@ -25,8 +25,8 @@ using Rhino.PlugIns;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("7d142de6-7a37-4b73-961b-a5ef3f7bfbc8")]
-[assembly: AssemblyVersion("0.3.17.9028")]
-[assembly: AssemblyFileVersion("0.3.17.9028")]
-[assembly: AssemblyInformationalVersion("0.3.17.9028")]
+[assembly: AssemblyVersion("0.3.18.9031")]
+[assembly: AssemblyFileVersion("0.3.18.9031")]
+[assembly: AssemblyInformationalVersion("0.3.18.9031")]
 
 [assembly: TargetFramework(".NETFramework,Version=v4.8", FrameworkDisplayName = ".NET Framework 4.8")]
